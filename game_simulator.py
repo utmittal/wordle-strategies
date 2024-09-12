276fee677e8c0ef21b5126a349b7d33953af6cb9
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-12 23:20:50 +1000

    Format code.

--- game_simulator.py
+++ game_simulator.py
@@ -4,29 +4,30 @@ from wordle_dictionary import WordleDictionary
 from termcolor import colored
 import os
 
+
 class State(StrEnum):
     blank = 'white'
     grey = 'grey'
     yellow = 'yellow'
     green = 'green'
 
+
 class GameSimulator:
     def __init__(self):
         self.__secret_word = None
-        self.__game_state = [[('_',State.yellow)]*5]*6
+        self.__game_state = [[('_', State.blank)] * 5] * 6
 
     def start_game(self):
         wd = WordleDictionary()
         self.__secret_word = wd.get_word()
 
     def show_board(self):
-        # enables colours on windows terminals
+        # enables colours on Windows terminals
         os.system('color')
 
         for row in self.__game_state:
-            coloured_row = [colored(e[0],color=e[1].value,force_color=True) for e in row]
+            coloured_row = [colored(e[0], color=e[1].value, force_color=True) for e in row]
             print(' '.join(coloured_row))
-
 
 
 gs = GameSimulator()
