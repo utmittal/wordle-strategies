4bbd6abc1811fd1c04ed9bba63854bc14c0eb97c
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-27 04:06:49 +1000

    Add tests for get_repeated_letter_position_index

--- util/tests/test_indexers.py
+++ util/tests/test_indexers.py
@@ -1,4 +1,4 @@
-from util.indexers import get_letter_index, get_letter_position_index
+from util.indexers import get_letter_index, get_letter_position_index, get_repeated_letter_position_index
 
 
 def test_get_letter_index_single_word():
@@ -51,3 +51,65 @@ def test_get_letter_position_index_words_of_different_length():
     assert letter_index == {'t': {0: {'test'}, 3: {'test', 'poster'}}, 'e': {1: {'test'}, 4: {'poster'}},
                             's': {2: {'test', 'poster'}}, 'p': {0: {'poster'}}, 'o': {1: {'poster'}},
                             'r': {5: {'poster'}}}
+
+
+def test_get_repeated_letter_position_index_no_repeated_letters_returns_empty_index():
+    word_list = ['post']
+
+    letter_index = get_repeated_letter_position_index(word_list)
+
+    assert letter_index == {}
+
+
+def test_get_repeated_letter_position_index_single_word_with_double_letter():
+    word_list = ['test']
+
+    letter_index = get_repeated_letter_position_index(word_list)
+
+    assert letter_index == {'t': {(0, 3): {'test'}}}
+
+
+def test_get_repeated_letter_position_index_single_word_with_more_than_two_letters():
+    word_list = ['eevee']
+
+    letter_index = get_repeated_letter_position_index(word_list)
+
+    assert letter_index == {'e': {(0, 1): {'eevee'},
+                                  (0, 1, 3): {'eevee'},
+                                  (0, 1, 3, 4): {'eevee'},
+                                  (0, 1, 4): {'eevee'},
+                                  (0, 3): {'eevee'},
+                                  (0, 3, 4): {'eevee'},
+                                  (0, 4): {'eevee'},
+                                  (1, 3): {'eevee'},
+                                  (1, 3, 4): {'eevee'},
+                                  (1, 4): {'eevee'},
+                                  (3, 4): {'eevee'}}}
+
+
+def test_get_repeated_letter_position_index_multiple_words_with_varying_letters():
+    word_list = ['test', 'eevee', 'ozone']
+
+    letter_index = get_repeated_letter_position_index(word_list)
+
+    assert letter_index == {'e': {(0, 1): {'eevee'},
+                                  (0, 1, 3): {'eevee'},
+                                  (0, 1, 3, 4): {'eevee'},
+                                  (0, 1, 4): {'eevee'},
+                                  (0, 3): {'eevee'},
+                                  (0, 3, 4): {'eevee'},
+                                  (0, 4): {'eevee'},
+                                  (1, 3): {'eevee'},
+                                  (1, 3, 4): {'eevee'},
+                                  (1, 4): {'eevee'},
+                                  (3, 4): {'eevee'}},
+                            'o': {(0, 2): {'ozone'}},
+                            't': {(0, 3): {'test'}}}
+
+
+def test_get_repeated_letter_position_index_multiple_words_with_same_indices():
+    word_list = ['root', 'boot', 'hoot']
+
+    letter_index = get_repeated_letter_position_index(word_list)
+
+    assert letter_index == {'o': {(1, 2): {'root', 'boot', 'hoot'}}}
