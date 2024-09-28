08a69ab8d72b1dec07454b0e4047a0caea530910
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-28 15:14:49 +1000

    Change intersect_all to have two required arguments.
    This means we don't need to copy the first set anymore. For sets of size ~1,000,000, a quick benchmarking shows that the function is now ~20% faster.

--- util/set_operations.py
+++ util/set_operations.py
@@ -1,9 +1,9 @@
-def intersect_all(set1: set, *sets_to_intersect: set) -> set:
+def intersect_all(set1: set, set2: set, *more_sets: set) -> set:
     """
     Returns an intersection of all given sets. Ignores empty sets.
     """
-    filtered_set = set1.copy()
-    for other_set in sets_to_intersect:
+    filtered_set = set1.intersection(set2)
+    for other_set in more_sets:
         if len(other_set) > 0:
             filtered_set.intersection_update(other_set)
 
