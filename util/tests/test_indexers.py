7465a16f7c0b09e9ff9a466f977dbbe5e5b48ca6
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-28 14:57:59 +1000

    Reformat test asserts to be more readable.

--- util/tests/test_indexers.py
+++ util/tests/test_indexers.py
@@ -6,7 +6,11 @@ def test_get_letter_index_single_word():
 
     letter_index = get_letter_index(word_list)
 
-    assert letter_index == {'t': {'test'}, 'e': {'test'}, 's': {'test'}}
+    assert letter_index == {
+        't': {'test'},
+        'e': {'test'},
+        's': {'test'}
+    }
 
 
 def test_get_letter_index_multiple_words():
@@ -14,7 +18,13 @@ def test_get_letter_index_multiple_words():
 
     letter_index = get_letter_index(word_list)
 
-    assert letter_index == {'t': {'test', 'post'}, 'e': {'test'}, 's': {'test', 'post'}, 'p': {'post'}, 'o': {'post'}}
+    assert letter_index == {
+        't': {'test', 'post'},
+        'e': {'test'},
+        's': {'test', 'post'},
+        'p': {'post'},
+        'o': {'post'}
+    }
 
 
 def test_get_letter_index_words_of_different_lengths():
@@ -22,8 +32,14 @@ def test_get_letter_index_words_of_different_lengths():
 
     letter_index = get_letter_index(word_list)
 
-    assert letter_index == {'t': {'test', 'poster'}, 'e': {'test', 'poster'}, 's': {'test', 'poster'}, 'p': {'poster'},
-                            'o': {'poster'}, 'r': {'poster'}}
+    assert letter_index == {
+        't': {'test', 'poster'},
+        'e': {'test', 'poster'},
+        's': {'test', 'poster'},
+        'p': {'poster'},
+        'o': {'poster'},
+        'r': {'poster'}
+    }
 
 
 def test_get_letter_position_index_single_word():
@@ -31,7 +47,18 @@ def test_get_letter_position_index_single_word():
 
     letter_index = get_letter_position_index(word_list)
 
-    assert letter_index == {'t': {0: {'test'}, 3: {'test'}}, 'e': {1: {'test'}}, 's': {2: {'test'}}}
+    assert letter_index == {
+        't': {
+            0: {'test'},
+            3: {'test'}
+        },
+        'e': {
+            1: {'test'}
+        },
+        's': {
+            2: {'test'}
+        }
+    }
 
 
 def test_get_letter_position_index_multiple_words():
@@ -39,8 +66,24 @@ def test_get_letter_position_index_multiple_words():
 
     letter_index = get_letter_position_index(word_list)
 
-    assert letter_index == {'t': {0: {'test'}, 3: {'test', 'post'}}, 'e': {1: {'test'}}, 's': {2: {'test', 'post'}},
-                            'p': {0: {'post'}}, 'o': {1: {'post'}}}
+    assert letter_index == {
+        't': {
+            0: {'test'},
+            3: {'test', 'post'}
+        },
+        'e': {
+            1: {'test'}
+        },
+        's': {
+            2: {'test', 'post'}
+        },
+        'p': {
+            0: {'post'}
+        },
+        'o': {
+            1: {'post'}
+        }
+    }
 
 
 def test_get_letter_position_index_words_of_different_length():
@@ -48,9 +91,28 @@ def test_get_letter_position_index_words_of_different_length():
 
     letter_index = get_letter_position_index(word_list)
 
-    assert letter_index == {'t': {0: {'test'}, 3: {'test', 'poster'}}, 'e': {1: {'test'}, 4: {'poster'}},
-                            's': {2: {'test', 'poster'}}, 'p': {0: {'poster'}}, 'o': {1: {'poster'}},
-                            'r': {5: {'poster'}}}
+    assert letter_index == {
+        't': {
+            0: {'test'},
+            3: {'test', 'poster'}
+        },
+        'e': {
+            1: {'test'},
+            4: {'poster'}
+        },
+        's': {
+            2: {'test', 'poster'}
+        },
+        'p': {
+            0: {'poster'}
+        },
+        'o': {
+            1: {'poster'}
+        },
+        'r': {
+            5: {'poster'}
+        }
+    }
 
 
 def test_get_repeated_letter_position_index_no_repeated_letters_returns_empty_index():
@@ -66,7 +128,11 @@ def test_get_repeated_letter_position_index_single_word_with_double_letter():
 
     letter_index = get_repeated_letter_position_index(word_list)
 
-    assert letter_index == {'t': {(0, 3): {'test'}}}
+    assert letter_index == {
+        't': {
+            (0, 3): {'test'}
+        }
+    }
 
 
 def test_get_repeated_letter_position_index_single_word_with_more_than_two_letters():
@@ -74,17 +140,21 @@ def test_get_repeated_letter_position_index_single_word_with_more_than_two_lette
 
     letter_index = get_repeated_letter_position_index(word_list)
 
-    assert letter_index == {'e': {(0, 1): {'eevee'},
-                                  (0, 1, 3): {'eevee'},
-                                  (0, 1, 3, 4): {'eevee'},
-                                  (0, 1, 4): {'eevee'},
-                                  (0, 3): {'eevee'},
-                                  (0, 3, 4): {'eevee'},
-                                  (0, 4): {'eevee'},
-                                  (1, 3): {'eevee'},
-                                  (1, 3, 4): {'eevee'},
-                                  (1, 4): {'eevee'},
-                                  (3, 4): {'eevee'}}}
+    assert letter_index == {
+        'e': {
+            (0, 1): {'eevee'},
+            (0, 1, 3): {'eevee'},
+            (0, 1, 3, 4): {'eevee'},
+            (0, 1, 4): {'eevee'},
+            (0, 3): {'eevee'},
+            (0, 3, 4): {'eevee'},
+            (0, 4): {'eevee'},
+            (1, 3): {'eevee'},
+            (1, 3, 4): {'eevee'},
+            (1, 4): {'eevee'},
+            (3, 4): {'eevee'}
+        }
+    }
 
 
 def test_get_repeated_letter_position_index_multiple_words_with_varying_letters():
@@ -92,19 +162,27 @@ def test_get_repeated_letter_position_index_multiple_words_with_varying_letters(
 
     letter_index = get_repeated_letter_position_index(word_list)
 
-    assert letter_index == {'e': {(0, 1): {'eevee'},
-                                  (0, 1, 3): {'eevee'},
-                                  (0, 1, 3, 4): {'eevee'},
-                                  (0, 1, 4): {'eevee'},
-                                  (0, 3): {'eevee'},
-                                  (0, 3, 4): {'eevee'},
-                                  (0, 4): {'eevee'},
-                                  (1, 3): {'eevee'},
-                                  (1, 3, 4): {'eevee'},
-                                  (1, 4): {'eevee'},
-                                  (3, 4): {'eevee'}},
-                            'o': {(0, 2): {'ozone'}},
-                            't': {(0, 3): {'test'}}}
+    assert letter_index == {
+        'e': {
+            (0, 1): {'eevee'},
+            (0, 1, 3): {'eevee'},
+            (0, 1, 3, 4): {'eevee'},
+            (0, 1, 4): {'eevee'},
+            (0, 3): {'eevee'},
+            (0, 3, 4): {'eevee'},
+            (0, 4): {'eevee'},
+            (1, 3): {'eevee'},
+            (1, 3, 4): {'eevee'},
+            (1, 4): {'eevee'},
+            (3, 4): {'eevee'}
+        },
+        'o': {
+            (0, 2): {'ozone'}
+        },
+        't': {
+            (0, 3): {'test'}
+        }
+    }
 
 
 def test_get_repeated_letter_position_index_multiple_words_with_same_indices():
@@ -112,4 +190,8 @@ def test_get_repeated_letter_position_index_multiple_words_with_same_indices():
 
     letter_index = get_repeated_letter_position_index(word_list)
 
-    assert letter_index == {'o': {(1, 2): {'root', 'boot', 'hoot'}}}
+    assert letter_index == {
+        'o': {
+            (1, 2): {'root', 'boot', 'hoot'}
+        }
+    }
