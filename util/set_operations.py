7054fd9a0cb27968371dbb7f1686d884bece405f
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-28 15:26:51 +1000

    Update intersect_all to handle corner cases where necessary arguments are empty sets.# Please enter the commit message for your changes. Lines starting

--- util/set_operations.py
+++ util/set_operations.py
@@ -2,9 +2,12 @@ def intersect_all(set1: set, set2: set, *more_sets: set) -> set:
     """
     Returns an intersection of all given sets. Ignores empty sets.
     """
-    filtered_set = set1.intersection(set2)
-    for other_set in more_sets:
-        if len(other_set) > 0:
-            filtered_set.intersection_update(other_set)
+    non_empty_sets = [s for s in [set1, set2, *more_sets] if len(s) > 0]
 
-    return filtered_set
+    if len(non_empty_sets) == 1:
+        return non_empty_sets[0]
+    else:
+        filtered_set = non_empty_sets[0].intersection(non_empty_sets[1])
+        for other_set in non_empty_sets[2:]:
+            filtered_set.intersection_update(other_set)
+        return filtered_set
--- util/tests/test_set_operations.py
+++ util/tests/test_set_operations.py
@@ -55,3 +55,34 @@ def test_intersect_all_doesnt_modify_arguments():
     assert set1 == {0, 1, 2}
     assert set2 == {0, 2, 3}
     assert set3 == {0, 1, 3}
+
+
+def test_intersect_all_set1_is_empty():
+    set1 = set()
+    set2 = {0, 1, 2}
+    set3 = {0, 3, 4}
+
+    intersection = intersect_all(set1, set2, set3)
+
+    assert intersection == {0}
+
+
+def test_intersect_all_set2_is_empty():
+    set1 = {0, 1, 2}
+    set2 = set()
+    set3 = {0, 3, 4}
+
+    intersection = intersect_all(set1, set2, set3)
+
+    assert intersection == {0}
+
+
+def test_intersect_all_set1_and_set2_is_empty():
+    set1 = set()
+    set2 = set()
+    set3 = {0, 3, 4}
+    set4 = {0, 2, 3}
+
+    intersection = intersect_all(set1, set2, set3, set4)
+
+    assert intersection == {0, 3}
